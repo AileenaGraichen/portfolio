92eb6fea6a42198eb690e1034a6acb89b0affc07
Aileena <aile0074@stud.kea.dk>
2023-03-13 14:19:04 +0100

    s

--- src/main/java/web/portfolio/controller/MainController.java
+++ src/main/java/web/portfolio/controller/MainController.java
@@ -2,6 +2,7 @@ package web.portfolio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
@@ -9,5 +10,17 @@ public class MainController {
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+    @RequestMapping("/projects")
+    public String projects(){
+        return "projects";
+    }
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "about";
     }
 }
